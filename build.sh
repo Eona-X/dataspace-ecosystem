b86786dcbad2350f0cb28c2ca62a82345177d93c
david <dlhullier@oxyl.fr>
2026-04-15 09:59:30 +0200

    All bump to 0.3.2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euxo
 #echo "${REGISTRY_PWD}" | docker login -u "${REGISTRY_USER}" --password-stdin "${REGISTRY_HOST}"
 echo $DOCKER_AUTH_CONFIG_DEV > ./gradle/config.json
 ./gradlew --gradle-user-home cache/ clean shadowJar dockerize dockerPush  \
-          -PimageTag=0.3.1 \
+          -PimageTag=0.3.2 \
           -PregistryHost=${REGISTRY_HOST} \
           -PregistryNs=eona-x/dse \
           -Pdocker.registry=${REGISTRY_HOST}/eona-x/dse \
